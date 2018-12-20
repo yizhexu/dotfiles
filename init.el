b4240cc0bd5ca56fbc004183f4fb5adecb9c9cb6
Yizhe <yizhe.xu@charter.com>
2018-12-19 19:09:58 -0700

    Modified init.el, removed configuratioion.el

--- init.el
+++ init.el
@@ -22,11 +22,7 @@
  '(org-agenda-files (quote ("~/Documents/org/index.org")))
  '(package-selected-packages
 	 (quote
-<<<<<<< HEAD
-		(htmlize org-bullets diff-hl multi-term ensime scala-mode jedi py-autopep8 evil-magit rainbow-delimiters paredit nord-theme go-eldoc auto-complete-config go-autocomplete auto-complete go-mode ess guide-key auto-compile use-package smart-mode-line multiple-cursors elpy minions pallet))))
-=======
 		(tramp-term neotree org-bullets diff-hl multi-term ensime scala-mode jedi py-autopep8 evil-magit rainbow-delimiters paredit nord-theme go-eldoc auto-complete-config go-autocomplete auto-complete go-mode ess guide-key auto-compile use-package smart-mode-line multiple-cursors elpy minions pallet))))
->>>>>>> de4611aa3fad7f7a8b450458ec66f3b197925deb
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
