2ba8c852923f68fdda5143d2ded667358f018fcd
Yizhe <yizhe.xu@charter.com>
2018-12-20 20:48:41 -0700

    Made some small change for it to work right on mac today

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+
 ;; initialize package.el packages
 (package-initialize)
 
@@ -12,16 +13,14 @@
 (org-babel-load-file
  (expand-file-name "configuration.org"
                    user-emacs-directory))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(initial-frame-alist (quote ((fullscreen . fullheight))))
- '(org-agenda-files (quote ("~/Documents/org/index.org")))
  '(package-selected-packages
-	 (quote )))
+	 (quote
+		(htmlize use-package tramp-term smart-mode-line rainbow-delimiters py-autopep8 paredit pallet org-bullets nord-theme neotree multiple-cursors multi-term minions material-theme jedi guide-key go-mode go-autocomplete flycheck evil-magit ess ensime elpy diff-hl auto-compile))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
