29888d61f9856cb03b521b72f3f48fa224833ba6
Yizhe <yizhe.xu@charter.com>
2019-01-13 07:09:23 -0700

    Python configuration

--- init.el
+++ init.el
@@ -19,8 +19,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-	 (quote
-		(material use-package tramp-term smart-mode-line rainbow-delimiters py-autopep8 paredit pallet org-bullets nord-theme neotree multiple-cursors multi-term minions material-theme julia-repl jedi htmlize guide-key go-mode go-autocomplete flycheck evil-magit ess ensime elpy diff-hl company-jedi auto-compile))))
+   (quote
+    (smartparens color-identifiers-mode material use-package tramp-term smart-mode-line rainbow-delimiters py-autopep8 paredit pallet org-bullets nord-theme neotree multiple-cursors multi-term minions material-theme julia-repl jedi htmlize guide-key go-mode go-autocomplete flycheck evil-magit ess ensime elpy diff-hl company-jedi auto-compile))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
