942a05b5aed0017a84dfcef8e34b007298e113a5
Yizhe <yizhe.xu@charter.com>
2019-02-17 22:30:30 -0700

    Update theme!

--- init.el
+++ init.el
@@ -11,17 +11,20 @@
 
 ;; load literal configuration
 (org-babel-load-file
-  (expand-file-name "configuration.org"
-                    user-emacs-directory))
+ (expand-file-name "configuration.org"
+                   user-emacs-directory))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-)
+ '(package-selected-packages
+   (quote
+    (cherry-blossom-theme monokai-theme use-package tramp-term toc-org smartparens smart-mode-line rainbow-delimiters py-autopep8 paredit pallet org-ref org-plus-contrib org-bullets org nord-theme neotree multiple-cursors multi-term minions material-theme julia-repl jedi guide-key go-eldoc go-autocomplete flycheck evil-magit ess ensime elpy diff-hl company-jedi company-go color-identifiers-mode auto-compile))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-)
+ )
