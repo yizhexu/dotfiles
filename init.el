db0389681c7eae27b676413c771cdbef0a430982
Yizhe <yizhe.xu@charter.com>
2019-03-25 10:38:56 -0600

    Rearranged org inbox a bit

--- init.el
+++ init.el
@@ -19,9 +19,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/Documents/megrez/index.org")))
  '(package-selected-packages
-	 (quote
-		(sqlup-mode cherry-blossom-theme use-package tramp-term toc-org smartparens smart-mode-line rainbow-delimiters py-autopep8 paredit pallet org-ref org-plus-contrib org-bullets org nord-theme neotree multiple-cursors multi-term material-theme julia-repl jedi guide-key go-eldoc go-autocomplete flycheck evil-magit ess ensime elpy diff-hl company-jedi company-go color-identifiers-mode auto-compile))))
+   (quote
+    (sqlup-mode cherry-blossom-theme use-package tramp-term toc-org smartparens smart-mode-line rainbow-delimiters py-autopep8 paredit pallet org-ref org-plus-contrib org-bullets org nord-theme neotree multiple-cursors multi-term material-theme julia-repl jedi guide-key go-eldoc go-autocomplete flycheck evil-magit ess ensime elpy diff-hl company-jedi company-go color-identifiers-mode auto-compile))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
