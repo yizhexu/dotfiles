3eb80fe223dc04fe38ac84aaef6a334c7df39e41
Yizhe <yizhe.xu@charter.com>
2019-03-25 20:59:39 -0600

    Update config

--- init.el
+++ init.el
@@ -19,9 +19,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   (quote
+    ("~/Documents/side-project/credential-sharing-paper/paper.org" "~/Documents/megrez/index.org" "~/Documents/megrez/projects.org" "~/Documents/megrez/tickler.org")))
  '(package-selected-packages
-	 (quote
-		(cherry-blossom-theme use-package tramp-term toc-org smartparens smart-mode-line rainbow-delimiters py-autopep8 paredit pallet org-ref org-plus-contrib org-bullets org nord-theme neotree multiple-cursors multi-term material-theme julia-repl jedi guide-key go-eldoc go-autocomplete flycheck evil-magit ess ensime elpy diff-hl company-jedi company-go color-identifiers-mode auto-compile))))
+   (quote
+    (cherry-blossom-theme use-package tramp-term toc-org smartparens smart-mode-line rainbow-delimiters py-autopep8 paredit pallet org-ref org-plus-contrib org-bullets org nord-theme neotree multiple-cursors multi-term material-theme julia-repl jedi guide-key go-eldoc go-autocomplete flycheck evil-magit ess ensime elpy diff-hl company-jedi company-go color-identifiers-mode auto-compile))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
