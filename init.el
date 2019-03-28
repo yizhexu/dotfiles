7975ee0cb22ca23954d5bb7f0a1b8628b050eee1
Yizhe <yizhe.xu@charter.com>
2019-03-28 02:30:25 -0600

    Update config etc

--- init.el
+++ init.el
@@ -1,25 +1,22 @@
-
-;; initialize package.el packages
-(package-initialize)
-
-;; Use cask & pallet to manage packages
-(require 'cask "~/.cask/cask.el")
-(cask-initialize)
-
-(require 'pallet)
-(pallet-mode t)
+;; Load the rest of the packages
+(package-initialize t)
+(setq package-enable-at-startup nil)
 
 ;; load literal configuration
 (org-babel-load-file
- (expand-file-name "configuration.org"
+ (expand-file-name "config.org"
                    user-emacs-directory))
 
+;;; init.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(debug-on-error t)
+ '(package-selected-packages
+   (quote
+    (aggressive-indent emojify beginend selected interaction-log discover-my-major help-fns help-fns+ which-key company-jedi jedi elpy py-autopep8 pyvenv esh-autosuggest company-eshell-autosuggest neotree org-bullets nord-theme use-package paradox org))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
