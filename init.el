6bd936cc643aefe66b1a94876fd42f2b8b77be3a
Yizhe <yizhe.xu@charter.com>
2019-03-28 18:49:45 -0600

    Updated config for agenda

--- init.el
+++ init.el
@@ -6,8 +6,6 @@
 (org-babel-load-file
  (expand-file-name "config.org"
                    user-emacs-directory))
-
-;;; init.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -16,10 +14,13 @@
  '(debug-on-error t)
  '(package-selected-packages
    (quote
-    (aggressive-indent emojify beginend selected interaction-log discover-my-major help-fns help-fns+ which-key company-jedi jedi elpy py-autopep8 pyvenv esh-autosuggest company-eshell-autosuggest neotree org-bullets nord-theme use-package paradox org))))
+    (origami org-super-agenda flycheck-clojure aggressive-indent emojify beginend selected interaction-log discover-my-major help-fns help-fns+ which-key company-jedi jedi elpy py-autopep8 pyvenv esh-autosuggest company-eshell-autosuggest neotree org-bullets nord-theme use-package paradox org))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-done ((t (:background "#E8E8E8" :foreground "#0E0E0E" :strike-through t :weight bold))))
+ '(org-headline-done ((t (:foreground "#171717" :strike-through t))))
+ '(org-image-actual-width (quote (600)))
+ '(variable-pitch ((t (:family "ETBembo")))))
