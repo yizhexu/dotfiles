f5a236aa888ab86f43a53d3ed0c6675e5e7755e9
Yizhe <yizhe.xu@charter.com>
2019-03-31 15:30:18 -0600

    Configure org babel, ref.

--- init.el
+++ init.el
@@ -6,3 +6,20 @@
 (org-babel-load-file
  (expand-file-name "config.org"
                    user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (julia-repl which-key visual-regexp use-package smartparens selected rainbow-delimiters py-autopep8 pcmpl-git paradox origami org-super-agenda org-gcal org-cliplink org-bullets nord-theme neotree magit linum-relative jedi interaction-log git-timemachine git-link git-auto-commit-mode flycheck-clojure f expand-region ess esh-autosuggest ensime emojify elpy elisp-slime-nav discover-my-major company-jedi beginend aggressive-indent))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-done ((t (:strike-through t))))
+ '(org-headline-done ((t (:strike-through t))))
+ '(org-image-actual-width (quote (600)))
+ '(variable-pitch ((t (:family "ETBembo")))))
