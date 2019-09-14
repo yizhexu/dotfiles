1fa1eb1c4e2e38a9a084a30515c1c60d70253f99
Yizhe Xu <me@yizhexu.com>
2019-09-14 13:47:19 -0700

    initiated config file for my termux config file

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -3,8 +3,9 @@
 (setq package-enable-at-startup nil)
 
 ;; load literal configuration
-(org-babel-load-file
- (expand-file-name "config.org" user-emacs-directory))
+(if (string-equal user-login-name "u0_a229")
+  (org-babel-load-file (expand-file-name "config-mobile.org" user-emacs-directory))
+  (org-babel-load-file (expand-file-name "config.org" user-emacs-directory)))
 
 ;; load additional configuration
 ;; (cond ((is-yizhx) (org-babel-load-file (expand-file-name "additional-config.org" user-emacs-directory))))
