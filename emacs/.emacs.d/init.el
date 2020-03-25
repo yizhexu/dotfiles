38888c82b62246874d435297f018c14c5bdcb68b
Yizhe <yizhx@amazon.com>
2020-03-24 19:06:19 -0700

    config update

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2,24 +2,8 @@
 (package-initialize t)
 (setq package-enable-at-startup nil)
 
-;; Straight
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-
 ;; load literal configuration
-(if (string-equal system-name "localhost")
-    (org-babel-load-file (expand-file-name "config-mobile.org" user-emacs-directory))
-  (org-babel-load-file (expand-file-name "config.org" user-emacs-directory)))
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 
 ;; load additional configuration
 ;; (cond ((is-yizhx) (org-babel-load-file (expand-file-name "additional-config.org" user-emacs-directory))))
